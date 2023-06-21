4a589020754ccd9b8bbce20ad98bf28f2fb44aac
Darkmyes <darkmyes@gmail.com>
2023-06-20 21:14:02 -0500

    Updates

---
+++ precache-manifest.fc9c9f7be514723e9bcd9ae713cd8c4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f9fd6445fa46b3269f0",
+    "url": "/pokedex/static/css/main.b17569ce.chunk.css"
+  },
+  {
+    "revision": "8f9fd6445fa46b3269f0",
+    "url": "/pokedex/static/js/main.8f9fd644.chunk.js"
+  },
+  {
+    "revision": "f1363c6ecd79109b20d0",
+    "url": "/pokedex/static/js/1.f1363c6e.chunk.js"
+  },
+  {
+    "revision": "69bab70dd00f4429b781",
+    "url": "/pokedex/static/js/runtime~main.69bab70d.js"
+  },
+  {
+    "revision": "4d5b3e7a6170b99f24857ec97841a0ea",
+    "url": "/pokedex/index.html"
+  }
+];
